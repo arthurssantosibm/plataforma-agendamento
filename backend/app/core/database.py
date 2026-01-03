65a19cf8ea67f95204b5b90b88b58d7a36cab08c
Arthur Santana dos Santos <arthurssantos@ibm.com>
2026-01-03 16:40:19 -0300

    update

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,19 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 import os
 
 load_dotenv()
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = (
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
 
-engine = create_engine(DATABASE_URL)
-
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,               
+    pool_pre_ping=True,       
+    connect_args={
+        "sslmode": "require"  
+    }
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -21,9 +33,7 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
@@ -32,11 +42,9 @@ def get_db():
     finally:
         db.close()
 
-
-
 if __name__ == "__main__":
     try:
-        with engine.connect():
-            print("✅ Conexão via SQLAlchemy com PostgreSQL realizada com sucesso!")
+        with engine.connect() as conn:
+            print("✅ Conexão via SQLAlchemy com PostgreSQL (AWS) realizada com sucesso!")
     except Exception as e:
         print("❌ Erro ao conectar no banco:", e)
